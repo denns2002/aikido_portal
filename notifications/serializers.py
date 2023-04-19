1d0b4eca891e4a6e8112f8d27ca3a7adcca7f1a6
Denis <denis.israfilov2002@mail.ru>
2023-04-19 11:00:57 +0500

    end notifications

---
+++ notifications/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from notifications.models import Notification, UserNotification
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = '__all__'
+
+
+class UserNotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserNotification
+        fields = '__all__'
+
+
+class UserNotificationListSerializer(UserNotificationSerializer):
+    notifications = NotificationSerializer()
--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, GenericAPIView
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 
-# Create your views here.
+from notifications.models import UserNotification, Notification
+from notifications.serializers import UserNotificationSerializer, \
+    NotificationSerializer, UserNotificationListSerializer
+
+
+class UserNotificationListView(ListModelMixin, GenericAPIView):
+    serializer_class = UserNotificationListSerializer
+    get = ListCreateAPIView.get
+
+    def get_queryset(self):
+        return UserNotification.objects.filter(users=self.request.user).order_by("-notifications__timestamp")
+
+
+class NotificationCreateView(CreateModelMixin, GenericAPIView):
+    serializer_class = NotificationSerializer
+    queryset = Notification.objects.all()
+    post = ListCreateAPIView.post
+
+
+class NotificationConnectView(CreateModelMixin, GenericAPIView):
+    serializer_class = UserNotificationSerializer
+    post = ListCreateAPIView.post
+
+    def get_queryset(self):
+        return UserNotification.objects.all()
+
