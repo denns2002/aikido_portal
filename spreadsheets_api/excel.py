10799b89f0054dbc02ae46a8f2d3880b3e30d0e0
Makdol <maxim190802@bk.ru>
2023-05-04 16:32:46 +0500

    get data from xlsx

--- spreadsheets_api/excel.py
+++ spreadsheets_api/excel.py
@@ -1,10 +1,23 @@
 import pandas as pd
 
-
 file = "excel.xlsx"
 xl = pd.ExcelFile(file)
-print(xl.sheet_names)
-df1 = xl.parse('Лист1')
+df1 = xl.parse(xl.sheet_names[0])
 keys = df1.keys()
-for i in keys:
-    print(df1[i][6-2])
+
+
+def get_sheet_data(dataframe):
+    data = []
+    key_list = []
+    for key in keys:
+        key_list.append(key)
+    data.append(key_list)
+    for row in range(len(dataframe)):
+        row_data = []
+        for columns in range(len(keys)):
+            row_data.append(dataframe[keys[columns]][row])
+        data.append(row_data)
+    print(data)
+
+
+get_sheet_data(df1)
